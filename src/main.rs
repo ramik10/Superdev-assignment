90b1cd5f74736e9ae5c2a25eb1dc870e4c5e5138
Ramik Mukherjee <ramik.14062000@gmail.com>
2025-07-01 02:37:57 +0530

    port binding issue

--- src/main.rs
+++ src/main.rs
@@ -308,7 +308,7 @@ async fn main() -> std::io::Result<()> {
             .service(send_sol)
             .service(send_token)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }   
